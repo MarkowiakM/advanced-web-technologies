2dcd1d8d99b07a31f73765bc6b1fc408d5d8f946
Paulina Drzazga <paulina@halpress.team>
2023-03-27 00:22:05 +0200

    Added dependencies for swagger

--- 4_spring_rest_api/SpringRestAPI/src/main/java/com/example/SpringRestAPI/SpringRestApiApplication.java
+++ 4_spring_rest_api/SpringRestAPI/src/main/java/com/example/SpringRestAPI/SpringRestApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = { "com.example.SpringRestAPI.*" })
-//@EnableSwagger2
 @SpringBootApplication
 public class SpringRestApiApplication {
 
